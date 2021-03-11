70f6a880a2e4b1bbb1a4d525003a0d0f329aa34f
Dan Goscomb <dan.goscomb@hso.co.uk>
2021-03-11 09:40:39 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const fs = require('fs');
+const axios = require('axios');
+const FormData = require('form-data');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const folder = core.getInput('jsfolder');
+    if(!fs.existsSync(folder)) {
+      throw {message: `Folder ${folder} does not exist`};
+    }
+    fs.readdir(folder, (err, files) => {
+      files.forEach(file => {
+        if(file.endsWith('.map')) {
+          const formData = new FormData();
+          formData.append('service_version', core.getInput('service-version'));
+          formData.append('service_name', core.getInput('service-name'));
+          formData.append('bundle_filepath', `${core.getInput('server-path')}/${file.slice(0, -4)}`);
+          formData.append('sourcemap', fs.createReadStream(`${folder}/${file}`, 'utf8'));
+          let headers = formData.getHeaders();
+          headers['Authorization'] = `Bearer ${core.getInput('apm-token')}`;
+          axios.post(
+            `${core.getInput('apm-server')}/assets/v1/sourcemaps`,
+            formData,
+            {
+              maxContentLength: Infinity,
+              maxBodyLength: Infinity,
+              headers: headers
+            }
+          ).then((data) => {
+            if(data.status === 202) {
+              core.info(`Uploaded ${file} for ${core.getInput('server-path')}/${file.slice(0, -4)}`);
+            }
+            else {
+              core.warning(`Upload of ${file} resulted in a ${data.status} return code instead of the expected 202`);
+            }
+          }).catch((err) => {
+            core.error(`Failed to upload ${file}: ${err.toString()}`);
+          });
+        }
+      });
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
